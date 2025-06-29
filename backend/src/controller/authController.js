24aaf8b1b1b4362f57eaf1e49fbf8a8f62e02fe6
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-06-30 00:10:09 +0800

    change logout cookie logic

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -82,8 +82,16 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logged out successfully" });
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
   } catch (error) {
     console.log("Error in logout controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
