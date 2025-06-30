16ae7b2a9f4864c9ce30b1cc6cbeaf8f8ee74657
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-06-30 10:29:37 +0800

    add epress json limit to 5mb

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 connectDB();
+
 app.use(cookieParser());
 app.use(
   cors({
@@ -15,8 +16,7 @@ app.use(
     credentials: true,
   })
 );
-
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
